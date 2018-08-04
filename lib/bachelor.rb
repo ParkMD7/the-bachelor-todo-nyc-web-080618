ebff51d9de4ebb5f029432e963a01e819746af42
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-08-04 19:32:11 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -80,7 +80,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |cont|
     #binding.pry
       cont.each do |season_key, season_value|
-        binding.pry
+        #binding.pry
           if season_value == "age"
             cont_counter+=1
             cont_ages+=value.to_f
