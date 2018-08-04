886b70372b1af115887cd656172ce0101c0d73b1
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-08-04 19:27:42 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -83,7 +83,7 @@ def get_average_age_for_season(data, season)
         #binding.pry
           cont.each do |season_key, season_value|
             #binding.pry
-              if season_value == hometown
+              if season_value == "age"
                 hometown_counter+=1
               end
           end
